8579782ead8700a2ef0d65a9c52460cede6799e2
Juan Gonzalez <JGonzalez506@protonmail.com>
2026-05-20 13:00:18 -0600

    Carga de BD inicial

--- funciones.py
+++ funciones.py
@@ -5,10 +5,166 @@
 
 #importaciones
 import re
+import pickle
 import tkinter as tk
+from datetime import datetime
 
 #funciones
-
+#validaciones para datos cargados
+def validarNombreCompletoBD(pNombre):
+    if isinstance(pNombre, list) and len(pNombre) == 3:
+        if isinstance(pNombre[0], str) and isinstance(pNombre[1], str) and isinstance(pNombre[2], str):
+            nombre = pNombre[0]+" "+pNombre[1]+" "+pNombre[2]
+            nombre = nombre.title()
+            if re.match("^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$", nombre):
+                return True
+    return False
+def validarCedulaBD(pCedula):
+    if isinstance(pCedula, str):
+        if re.match("^[1-7]{1}-\\d{4}-\\d{4}$", pCedula):
+            return True
+    return False
+def validarTSangreBD(pSangre):
+    if pSangre in [1,2,3,4,5,6,7,8]:
+        return True
+    return False
+def validarSexoBD(pSexo):
+    if isinstance(pSexo, bool):
+        return True
+    return False
+def validarEdad(pFecha):
+    fecha1 = datetime(pFecha[2], pFecha[1], pFecha[0])
+    fecha2 = datetime.now()
+    edad = fecha2.year - fecha1.year
+    if (fecha2.month, fecha2.day) < (fecha1.month, fecha1.day):
+        edad -= 1
+    if edad >= 18 and edad < 65:
+        return True
+    return False
+def formatoFechaBD(diaNac,mesNac,annoNac):
+    if isinstance(diaNac, int) and isinstance(mesNac, int) and isinstance(annoNac, int):
+        if diaNac < 10:
+            dia = f"0{diaNac}"
+        else:
+            dia = f"{diaNac}"
+        if mesNac < 10:
+            mes = f"0{mesNac}"
+        else:
+            mes = f"{mesNac}"
+        anno = f"{annoNac}"
+    elif isinstance(diaNac, str) and isinstance(mesNac, str) and isinstance(annoNac, str):
+        if len(diaNac) == 1:
+            dia = f"0{diaNac}"
+        else:
+            dia = f"{diaNac}"
+        if len(mesNac) == 1:
+            mes = f"0{mesNac}"
+        else:
+            mes = f"{mesNac}"
+        anno = f"{annoNac}"
+    else:
+        return False
+    return f"{dia}/{mes}/{anno}",dia,mes,anno
+def validarFechaNacBD(pFechaNac):
+    if isinstance(pFechaNac, tuple):
+        diaNac = pFechaNac[0]
+        mesNac = pFechaNac[1]
+        annoNac = pFechaNac[2]
+        fechaNac,dia,mes,anno = formatoFechaBD(diaNac,mesNac,annoNac)
+        if not re.match("^\\d{2}/\\d{2}/\\d{4}$", fechaNac):
+            return False
+        if mes in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]:
+            diaNacInt = int(dia)
+            mesNacInt = int(mes)
+            annoNacInt = int(anno)
+            fechaNacInt = (diaNacInt,mesNacInt,annoNacInt)
+            estado = False
+            if mes in ["01", "03", "05", "07", "08", "10", "12"]:
+                if diaNacInt <= 31 and diaNacInt >= 1:
+                    estado = validarEdad(fechaNacInt)
+            elif mes in ["04", "06", "09", "11"]:
+                if diaNacInt <= 30 and diaNacInt >= 1:
+                    estado = validarEdad(fechaNacInt)
+            elif mes == "02":
+                esBisiesto = (annoNacInt % 4 == 0 and annoNacInt % 100 != 0) or (annoNacInt % 400 == 0)
+                if esBisiesto == True and 1 <= diaNacInt and diaNacInt <= 29:
+                    estado = validarEdad(fechaNacInt)
+                elif esBisiesto == False and 1 <= diaNacInt and diaNacInt <= 28:
+                    estado = validarEdad(fechaNacInt)
+            if estado:
+                return True
+        return False
+def validarPesoBD(pPeso):
+    if isinstance(pPeso, float):
+        if pPeso > 50 and pPeso < 120:
+            return True
+    return False
+def validarCorreoBD(pCorreo):
+    if isinstance(pCorreo, str):
+        patron = r"^[a-zA-Z0-9._%+-]+@(costarricense\.cr|racsa\.go\.cr|ccss\.sa\.cr|gmail+\.com)$"
+        if re.match(patron, pCorreo):
+            return True
+    return False
+def validarTelBD(pTel):
+    if isinstance(pTel, str):
+        patron = r"^\d{4}-\d{4}$"
+        if re.match(patron, pTel) and pTel[0] not in "0135":
+            return True
+    return False
+def validarEstadoBD(pEstado):
+    if pEstado in [0,1]:
+        return True
+    return False
+def validarJustificacionBD(pJust):
+    if pJust in [0,1,2,3,4,5,6,7]:
+        return True
+    return False
+def validarBD(bdDonadores):
+    if isinstance(bdDonadores, list):
+        validos = 0
+        invalidos = 0
+        temp = []
+        for donador in bdDonadores:
+            if isinstance(donador, list) and len(donador) == 10:
+                if validarNombreCompletoBD(donador[0]) and validarCedulaBD(donador[1]) and validarTSangreBD(donador[2]) and validarSexoBD(donador[3]) and validarFechaNacBD(donador[4]):
+                    if validarPesoBD(donador[5]) and validarCorreoBD(donador[6]) and validarTelBD(donador[7]) and validarEstadoBD(donador[8]) and validarJustificacionBD(donador[9]):
+                        fecha = donador[4]
+                        fecha = (f"{fecha[0]}",f"{fecha[1]}",f"{fecha[2]}")
+                        datos = [donador[0],donador[1],donador[2],donador[3],fecha,donador[5],donador[6],donador[7],donador[8],donador[9]]
+                        temp.append(datos)
+                        validos += 1
+                    else:
+                        invalidos += 1
+                else:
+                    invalidos += 1
+            else:
+                invalidos += 1
+        print(f"Válidos: {validos} - Invalidos: {invalidos}")
+        bdDonadores = temp
+        return bdDonadores
+    return False
+#Cargar base de datos
+def cargarDonadores():
+    try:
+        with open("bdDonadores.txt", "rb") as f:
+            bdDonadores = pickle.load(f)
+            print("Base de datos cargada")
+            # print(bdDonadores)
+            print()
+            print("Base de datos limpiada")
+            bdDonadores = validarBD(bdDonadores)
+        return bdDonadores
+    except:
+        print("Se ha inicializado la base de datos")
+        return [[]]
+#Guardar base de datos
+def guardarDonadores(bdDonadores):
+    with open("bdDonadores.txt", "wb") as f:
+        pickle.dump(bdDonadores, f)
+        print("Base de datos guardada")
+        # print(bdDonadores)
+    return
+#Ventana de Menu
 def volverMenu(ventanaActual, ventanaMenu):
     ventanaActual.destroy()
     ventanaMenu.deiconify()
@@ -24,12 +180,6 @@ def insertarDonador(ventanaMenu, anchoVentana, altoVentana, posicionX, posicionY
                                       font=("Arial", 12))
     mensIngresarDonador.place(x=340, y=30)
 
-
-
-
-
-
-
     mensCedula = tk.Label(ventanaIngresarDon,
                           text="Digite su cedula de la siguiente manera: #-####-####:")
     mensCedula.place(x=98, y=70)
@@ -38,11 +188,6 @@ def insertarDonador(ventanaMenu, anchoVentana, altoVentana, posicionX, posicionY
                             text="", 
                             font=("Arial", 10))
     mensCedulaValidacion.place(x=265, y=90)
-
-
-
-
-
 
     mensNombre = tk.Label(ventanaIngresarDon,
                              text="Digite su nombre:")
@@ -53,10 +198,6 @@ def insertarDonador(ventanaMenu, anchoVentana, altoVentana, posicionX, posicionY
                             font=("Arial", 10))
     mensNombreValidacion.place(x=265, y=138)
 
-
-
-
-
     mensFechaNacimiento = tk.Label(ventanaIngresarDon,
                              text="Digite fecha de nacimiento de la siguiente manera: DD/MM/AAAA:")
     mensFechaNacimiento.place(x=98, y=168)
@@ -66,23 +207,9 @@ def insertarDonador(ventanaMenu, anchoVentana, altoVentana, posicionX, posicionY
                             font=("Arial", 10))
     mensFechaNacValidacion.place(x=265, y=188)
 
-
-
-
-
-
-
     cedula = ingresarCedula(ventanaIngresarDon, mensCedulaValidacion)
     nombreDon = ingresarNombre(ventanaIngresarDon, mensNombreValidacion)
     fechaNamiento = ingresarFechaNac(ventanaIngresarDon, mensFechaNacValidacion)
-
-
-
-
-
-
-
-
 
 def ingresarCedula(ventanaIngresarDon, mensCedulaValidacion):
     cedulaDonador = tk.Entry(ventanaIngresarDon,
@@ -99,10 +226,6 @@ def validarCedula(cedula, mensCedulaValidacion):
     else:
         mensCedulaValidacion.config(text="Cedula invalida. debe estar escrita como en el ejemplo y solo datos caracteres numericos",
                                     foreground="red")
-
-
-
-
 
 def ingresarNombre(ventanaIngresarDon, mensNombreValidacion):
     nombreDonador = tk.Entry(ventanaIngresarDon,
@@ -121,10 +244,6 @@ def validarNombre(nombre, mensNombreValidacion):
         mensNombreValidacion.config(text="Nombre invalido. Su nombre debe estar conformado solo por letras",
                                     foreground="red")
 
-
-
-
-
 def ingresarFechaNac(ventanaIngresarDon, mensFechaNacValidacion):
     fechaNacDonador = tk.Entry(ventanaIngresarDon,
                          font=("Arial", 11))
@@ -134,7 +253,6 @@ def ingresarFechaNac(ventanaIngresarDon, mensFechaNacValidacion):
 
 def validarFechaNac(fechaNac, mensFechaNacValidacion):
     fechaNac = fechaNac.widget.get()
-
     if not re.match("^\\d{2}/\\d{2}/\\d{4}$", fechaNac):
         mensFechaNacValidacion.config(text="Fecha invalida. el formato debe ser DD/MM/AAAA",
                                       foreground="red")
@@ -144,12 +262,15 @@ def validarFechaNac(fechaNac, mensFechaNacValidacion):
     MesNac = fechaNac[1]
     annoNac = fechaNac[2]
     diaNacInt = int(diaNac)
-    MesNacInt = int(MesNac)
+    # MesNacInt = int(MesNac)
     annoNacInt = int(annoNac)
-
-    if MesNacInt <= 12 and MesNacInt >= 1:
-        if MesNac in "01030507081012":
+    if MesNac in ["01","02","03","04","05","06","07","08","09","10","11","12"]:
+        if MesNac in ["01","03","05","07","08","10","12"]:
             if diaNacInt <= 31 and diaNacInt >= 1:
+                mensFechaNacValidacion.config(text="Fecha valida",
+                                              foreground="green")
+        elif MesNac in ["04","06","09","11"]:
+            if diaNacInt <= 30 and diaNacInt >= 1:
                 mensFechaNacValidacion.config(text="Fecha valida",
                                               foreground="green")
         elif MesNac == "02":
@@ -164,10 +285,6 @@ def validarFechaNac(fechaNac, mensFechaNacValidacion):
             else:
                 mensFechaNacValidacion.config(text="Fecha invalida. Verifique que su fecha de nacimiento este escrita de forma correcta",
                                               foreground="red")
-    else:
-        if diaNacInt <= 30 and diaNacInt >= 1:
-            mensFechaNacValidacion.config(text="Fecha valida",
-                                              foreground="green")
         else:
             mensFechaNacValidacion.config(text="Fecha invalida. Verifique que su fecha de nacimiento este escrita de forma correcta",
                                               foreground="red")
--- menu.py
+++ menu.py
@@ -7,97 +7,76 @@
 import funciones
 import tkinter as tk
 
-
-
-
-
 #Menu
-ventanaMenu = tk.Tk()
-ventanaMenu.title("Sistema de Banco de Sangre")
+def menu():
+    ventanaMenu = tk.Tk()
+    ventanaMenu.title("Sistema de Banco de Sangre")
 
-anchoPantalla = ventanaMenu.winfo_screenwidth()
-altoPantalla = ventanaMenu.winfo_screenheight()
-anchoVentana = 800
-altoVentana = 600
-posicionX = round((anchoPantalla / 2) - (anchoVentana / 2))
-posicionY = round((altoPantalla / 2) - (altoVentana / 2))
-ventanaMenu.geometry(f"{anchoVentana}x{altoVentana}+{posicionX}+{posicionY}")
+    anchoPantalla = ventanaMenu.winfo_screenwidth()
+    altoPantalla = ventanaMenu.winfo_screenheight()
+    anchoVentana = 800
+    altoVentana = 600
+    posicionX = round((anchoPantalla / 2) - (anchoVentana / 2))
+    posicionY = round((altoPantalla / 2) - (altoVentana / 2))
+    ventanaMenu.geometry(f"{anchoVentana}x{altoVentana}+{posicionX}+{posicionY}")
 
-mensajeMenu = tk.Label(text="Estas en el Menu Pricipal. Presione el Boton de la Opcion que Desea",
-                       font=("Arial", 12))
-mensajeMenu.place(x=170, y=50)
+    mensajeMenu = tk.Label(text="Estas en el Menu Principal. Presione el Boton de la Opcion que Desea",
+                           font=("Arial", 12))
+    mensajeMenu.place(x=170, y=50)
 
+    opcIngresarDonante = tk.Button(ventanaMenu,
+                                   cursor="Hand2",
+                                   text="Ingresar Donante",
+                                   relief="groove",
+                                   font=("Arial", 11),
+                                   command=lambda: funciones.insertarDonador(ventanaMenu, anchoVentana, altoVentana, posicionX, posicionY))
+    opcIngresarDonante.place(x=100, y=100)
 
-opcIngresarDonante = tk.Button(ventanaMenu,
-                               cursor="Hand2",
-                               text="Ingresar Donante",
-                               relief="groove",
-                               font=("Arial", 11),
-                               command=lambda: funciones.insertarDonador(ventanaMenu, anchoVentana, altoVentana, posicionX, posicionY))
-opcIngresarDonante.place(x=100, y=100)
+    opcGenerarDonadores = tk.Button(ventanaMenu,
+                                    cursor="Hand2",
+                                    text="Generar Donadores",
+                                    relief="groove",
+                                    font=("Arial", 11))
+    opcGenerarDonadores.place(x=100, y=150)
 
+    opcActualizarDatosDonador = tk.Button(ventanaMenu,
+                                          cursor="Hand2",
+                                          text="Actualizar Dtos del Donador",
+                                          relief="groove",
+                                          font=("Arial", 11))
+    opcActualizarDatosDonador.place(x=100, y=200)
 
+    opcEliminarDonador = tk.Button(ventanaMenu,
+                                   cursor="Hand2",
+                                   text="Eliminar Donador",
+                                   relief="groove",
+                                   font=("Arial", 11))
+    opcEliminarDonador.place(x=100, y=250)
 
+    opcLugarDonacion = tk.Button(ventanaMenu,
+                                 cursor="Hand2",
+                                 text="Insertar lugar de donación según provincia",
+                                 relief="groove",
+                                 font=("Arial", 11))
+    opcLugarDonacion.place(x=100, y=300)
 
-opcGenerarDonadores = tk.Button(ventanaMenu,
-                                cursor="Hand2",
-                                text="Generar Donadores",
-                                relief="groove",
-                                font=("Arial", 11))
-opcGenerarDonadores.place(x=100, y=150)
+    opcReportes = tk.Button(ventanaMenu,
+                            cursor="Hand2",
+                            text="Reportes",
+                            relief="groove",
+                            font=("Arial", 11))
+    opcReportes.place(x=100, y=350)
 
+    opcSalir = tk.Button(ventanaMenu,
+                         cursor="Hand2",
+                         text="Salir",
+                         relief="groove",
+                         font=("Arial", 11))
+    opcSalir.place(x=100, y=400)
 
-
-
-opcActualizarDatosDonador = tk.Button(ventanaMenu,
-                                      cursor="Hand2",
-                                      text="Actualizar Dtos del Donador",
-                                      relief="groove",
-                                      font=("Arial", 11))
-opcActualizarDatosDonador.place(x=100, y=200)
-
-
-
-
-opcEliminarDonador = tk.Button(ventanaMenu,
-                               cursor="Hand2",
-                               text="Eliminar Donador",
-                               relief="groove",
-                               font=("Arial", 11))
-opcEliminarDonador.place(x=100, y=250)
-
-
-
-opcLugarDonacion = tk.Button(ventanaMenu,
-                             cursor="Hand2",
-                             text="Insertar lugar de donación según provincia",
-                             relief="groove",
-                             font=("Arial", 11))
-opcLugarDonacion.place(x=100, y=300)
-
-
-
-opcReportes = tk.Button(ventanaMenu,
-                        cursor="Hand2",
-                        text="Reportes",
-                        relief="groove",
-                        font=("Arial", 11))
-opcReportes.place(x=100, y=350)
-
-
-
-opcSalir = tk.Button(ventanaMenu,
-                     cursor="Hand2",
-                     text="Salir",
-                     relief="groove",
-                     font=("Arial", 11))
-opcSalir.place(x=100, y=400)
-
-
-
-
-ventanaMenu.mainloop()
-
+    ventanaMenu.mainloop()
 
 
 #Programa Principal
+bdDonadores = funciones.cargarDonadores()
+menu()
